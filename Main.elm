fb3b71ac9f6eaf6e2a456639ea4a5b54f2a9a3d0
Alex Spurling <alexspurling@gmail.com>
2015-11-19 23:29:18 +0000

    Adding some game state. Upgrading to Elm 0.16

--- Main.elm
+++ Main.elm
@@ -1,5 +1,6 @@
 module Main where
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Time exposing (..)
@@ -8,27 +9,65 @@ import Maybe
 
 import Logo exposing (Highlight)
 
-main = Signal.map renderGameState gameState
+main = Signal.map renderModel model
 
-type alias GameState =
-  { sequence : List Logo.Highlight
+type GameState
+  = Start
+  | Highlight
+  | Pause
+  | WaitForInput
+
+
+type alias Model =
+  { sequence : Array Logo.Highlight
+  , highlightIndex : Int -- The index in the array of the current highlight
+  , state : GameState
+  , highlightDuration : Int -- Number of game loops to do a highlight
+  , pauseDuration : Int -- Number of game loops between two highlights
+  , highlightStep : Int -- The game loop number tracking how long a highlight has been active for
+  , pauseStep : Int -- The game loop number tracking how long we have been paused for
   }
 
 defaultState = 
-  { sequence = [Logo.HYellow, Logo.HBlue, Logo.HPurple, Logo.HGreen] 
+  { sequence = Array.fromList [Logo.HYellow, Logo.HBlue, Logo.HPurple, Logo.HGreen] 
+  , highlightIndex = 0
+  , state = Start
+  , highlightDuration = 100 -- 1s
+  , pauseDuration = 10 -- 0.1s
+  , highlightStep = 0
+  , pauseStep = 0
   }
 
-gameState : Signal GameState
-gameState = Signal.foldp gameLoop defaultState (every second)
+model : Signal Model
+model = Signal.foldp gameLoop defaultState (every second)
 
-gameLoop : Time -> GameState -> GameState
-gameLoop time gameState =
-  { gameState | sequence <- List.drop 1 gameState.sequence }
+gameLoop : Time -> Model -> Model
+gameLoop time model =
+  case model.state of
+    Start ->
+      { model | state = Highlight } -- TODO start count down to first highlight
+    Highlight ->
+      let
+        highlightIndex = model.highlightIndex + 1
+        newState = 
+          if highlightIndex < Array.length model.sequence then Pause else WaitForInput
+      in
+        { model | 
+          highlightIndex = highlightIndex,
+          state = newState }
+    Pause ->
+      { model | state = Highlight }
+    WaitForInput ->
+      model
 
-renderGameState : GameState -> Html
-renderGameState gameState =
+renderModel : Model -> Html
+renderModel model =
   let
-    currentHighlight = Maybe.withDefault Logo.None (List.head gameState.sequence)
+    currentHighlight =
+      if model.state == Highlight then
+        Maybe.withDefault Logo.None (Array.get model.highlightIndex model.sequence)
+      else
+        Logo.None
   in
     renderBoard currentHighlight
 
