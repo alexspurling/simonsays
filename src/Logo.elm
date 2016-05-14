c526e598a2a40339bc5958e3eea87a04b0b6774a
Alex Spurling <alexspurling@gmail.com>
2016-05-14 15:17:25 +0100

    Added border to highlighted svg elements

--- src/Logo.elm
+++ src/Logo.elm
@@ -35,19 +35,45 @@ colour colour highlight =
     Purple ->
       if highlight == HPurple then "#3b3b97" else "#5A6378"
 
+edge : Colour -> Highlight -> Attribute Msg
+edge colour highlight =
+  let
+    edgeStyle =
+      case colour of
+        Yellow ->
+          if highlight == HYellow then "stroke:#ffc229;stroke-width:2" else ""
+        Green ->
+          if highlight == HGreen then "stroke:#8cfe2f;stroke-width:2" else ""
+        Blue ->
+          if highlight == HBlue then "stroke:#38d4ff;stroke-width:2" else ""
+        Purple ->
+          if highlight == HPurple then "stroke:#3b3b97;stroke-width:2" else ""
+  in
+    Svg.Attributes.style edgeStyle
+
 logo : Highlight -> Html Msg
 logo highlight =
-  svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
-    [ polygon [ fill (colour Yellow highlight), onClick (Click Yellow), points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
-    , polygon [ fill (colour Green highlight), onClick (Click Green), points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
-    , rect
-        [ fill (colour Green highlight), onClick (Click Green)
-        , x "192.99", y "107.392", width "107.676", height "108.167"
-        , transform "matrix(0.7071 0.7071 -0.7071 0.7071 186.4727 -127.2386)"
-        ]
-        []
-    , polygon [ fill (colour Blue highlight), onClick (Click Blue), points "323.298,143.724 323.298,0 179.573,0" ] []
-    , polygon [ fill (colour Purple highlight), onClick (Click Purple), points "152.781,161.649 0,8.868 0,314.432" ] []
-    , polygon [ fill (colour Yellow highlight), onClick (Click Yellow), points "255.522,246.655 323.298,314.432 323.298,178.879" ] []
-    , polygon [ fill (colour Blue highlight), onClick (Click Blue), points "161.649,170.517 8.869,323.298 314.43,323.298" ] []
-    ]
+  let
+    colourYellow = colour Yellow highlight
+    colourGreen = colour Green highlight
+    colourBlue = colour Blue highlight
+    colourPurple = colour Purple highlight
+    edgeYellow = edge Yellow highlight
+    edgeGreen = edge Green highlight
+    edgeBlue = edge Blue highlight
+    edgePurple = edge Purple highlight
+  in
+    svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
+      [ polygon [ edgeYellow, fill colourYellow, onClick (Click Yellow), points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
+      , polygon [ edgeGreen, fill colourGreen, onClick (Click Green), points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
+      , rect
+          [ edgeGreen, fill colourGreen, onClick (Click Green)
+          , x "192.99", y "107.392", width "107.676", height "108.167"
+          , transform "matrix(0.7071 0.7071 -0.7071 0.7071 186.4727 -127.2386)"
+          ]
+          []
+      , polygon [ edgeBlue, fill colourBlue, onClick (Click Blue), points "323.298,143.724 323.298,0 179.573,0" ] []
+      , polygon [ edgePurple, fill colourPurple, onClick (Click Purple), points "152.781,161.649 0,8.868 0,314.432" ] []
+      , polygon [ edgeYellow, fill colourYellow, onClick (Click Yellow), points "255.522,246.655 323.298,314.432 323.298,178.879" ] []
+      , polygon [ edgeBlue, fill colourBlue, onClick (Click Blue), points "161.649,170.517 8.869,323.298 314.43,323.298" ] []
+      ]
