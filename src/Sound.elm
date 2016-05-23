465d0f23ab1c4002847c03493991b0e8967a9a44
Alex Spurling <alexspurling@gmail.com>
2016-05-23 20:14:27 +0100

    Fixing paths to sounds

--- src/Sound.elm
+++ src/Sound.elm
@@ -11,11 +11,11 @@ type Note = G3 | C4 | E4 | G4 | Nope
 soundUrl : Note -> String
 soundUrl note =
   case note of
-    G3 -> "/sound/g3.wav"
-    C4 -> "/sound/c4.wav"
-    E4 -> "/sound/e4.wav"
-    G4 -> "/sound/g4.wav"
-    Nope -> "/sound/nope.wav"
+    G3 -> "sound/g3.wav"
+    C4 -> "sound/c4.wav"
+    E4 -> "sound/e4.wav"
+    G4 -> "sound/g4.wav"
+    Nope -> "sound/nope.wav"
 
 
 initialiseSounds : Cmd msg
